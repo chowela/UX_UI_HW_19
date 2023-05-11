73ac80bb90fc77a9f02fdef23f968b2bd054fe6e
chowela <118566645+chowela@users.noreply.github.com>
2023-05-11 18:31:39 -0400

    new

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 
-$(".Resume").on("mouseover", function (){
-    $(this).css ("background-color", "#2E2E2E" );
-    $(this).css ("color", "#74D1BF");
-    console.log ("this is workingngg pls")
+$(".btn").hover(function(){
+    $(this).css("background-color", "#2E2E2E");
+    $(this).css("color", "#74D1BF");
+},
+function(){
+    $(this).css("background-color", "#74D1BF");
+    $(this).css("color", "#2E2E2E");
+});
+
+$(".caseStudy").hover(function(){
+    $(this).css("background-color", "#2E2E2E");
+    $(this).css("color", "#E8BA28");
+},
+function(){
+    $(this).css("background-color", "#D9D9D9");
+    $(this).css("color", "#2E2E2E");
 });
