7b1e8e49c0728ecf7e5f0d3ba762365dd2944375
chowela <118566645+chowela@users.noreply.github.com>
2023-05-11 16:27:19 -0400

    update

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+$(".Resume").on("mouseover", function (){
+    $(this).css ("background-color", "#2E2E2E" );
+    $(this).css ("color", "#74D1BF");
+    console.log ("this is workingngg pls")
+});
