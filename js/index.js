efc27df8dad65e9d4a0ea89fb4e0c5cee986ae40
chowela <118566645+chowela@users.noreply.github.com>
2023-05-18 16:25:28 -0400

    update

--- js/index.js
+++ js/index.js
@@ -16,3 +16,12 @@ function(){
     $(this).css("background-color", "#D9D9D9");
     $(this).css("color", "#2E2E2E");
 });
+
+$(".caseStudy a").hover(function(){
+    $(this).css("background-color", "#2E2E2E");
+    $(this).css("color", "#E8BA28");
+},
+function(){
+    $(this).css("background-color", "#D9D9D9");
+    $(this).css("color", "#2E2E2E");
+});
